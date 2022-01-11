66e0d6d22815de696f01e91b4d2ceca212e0020c
jbrophy <jonathanbrophy47@gmail.com>
2022-01-11 13:43:40 -0800

    added fold arg to batch

--- jobs/pp/batch.sh
+++ jobs/pp/batch.sh
@@ -3,8 +3,12 @@ o='jobs/logs/pp/'
 t='lgb'
 b='none'
 
-sbatch -a 1-16  -c 10  -t 1440 -p 'short' -o ${o}'constant-%a.out' $run 'constant' $t $b
-sbatch -a 1-16  -c 10  -t 1440 -p 'short' -o ${o}'knn-%a.out'      $run 'knn' $t $b
-sbatch -a 1-16  -c 10  -t 1440 -p 'short' -o ${o}'kgbm-%a.out'     $run 'kgbm' $t $b
-sbatch -a 1-16  -c 10  -t 1440 -p 'short' -o ${o}'ngboost-%a.out'  $run 'ngboost' $t $b
-sbatch -a 1-16  -c 10  -t 1440 -p 'short' -o ${o}'pgbm-%a.out'     $run 'pgbm' $t $b
+fold_list=(1 2 3 4 5)
+
+for f in ${fold_list[@]}; do
+    sbatch -a 1-15 -c 10 -t 1440 -p 'short' -o ${o}'constant-%a.out' $run $f 'constant' $t $b
+    sbatch -a 1-15 -c 10 -t 1440 -p 'short' -o ${o}'knn-%a.out'      $run $f 'knn'      $t $b
+    sbatch -a 1-15 -c 10 -t 1440 -p 'short' -o ${o}'kgbm-%a.out'     $run $f 'kgbm'     $t $b
+    sbatch -a 1-15 -c 10 -t 1440 -p 'short' -o ${o}'ngboost-%a.out'  $run $f 'ngboost'  $t $b
+    sbatch -a 1-15 -c 10 -t 1440 -p 'short' -o ${o}'pgbm-%a.out'     $run $f 'pgbm'     $t $b
+done
--- jobs/pp/runner.sh
+++ jobs/pp/runner.sh
@@ -6,9 +6,10 @@
 module load miniconda
 conda activate jbrophy-20210713
 
-model=$1
-tree_type=$2
-scale_bias=$3
+fold=$1
+model=$2
+tree_type=$3
+scale_bias=$4
 
 . jobs/config.sh
 
@@ -16,6 +17,7 @@ dataset=${datasets[${SLURM_ARRAY_TASK_ID}]}
 
 python3 scripts/experiments/prediction.py \
   --dataset $dataset \
+  --fold $fold \
   --model $model \
   --tree_type $tree_type \
   --scale_bias $scale_bias \
