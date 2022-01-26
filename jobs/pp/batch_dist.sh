d8d8119d1cefd2bcc6ec2bce702f813d4711d551
jbrophy <jonathanbrophy47@gmail.com>
2022-01-25 19:56:03 -0800

    fixing argument name

--- jobs/pp/batch_dist.sh
+++ jobs/pp/batch_dist.sh
@@ -7,8 +7,8 @@ c='dist'
 fold_list=(1 2 3 4 5)
 
 for f in ${fold_list[@]}; do
-    sbatch -a 1-10,12-19,21-22 -c 10 -t 1440  -p 'short' -o ${o}'kgbm-%a.out' $run $f 'kgbm' $tree $d $g $c
-    sbatch -a 11,20            -c 10 -t 7200  -p 'long'  -o ${o}'kgbm-%a.out' $run $f 'kgbm' $tree $d $g $c
+    sbatch -a 1-10,12-19,21-22 -c 10 -t 1440  -p 'short' -o ${o}'kgbm-%a.out' $run $f 'kgbm' $t $d $g $c
+    sbatch -a 11,20            -c 10 -t 7200  -p 'long'  -o ${o}'kgbm-%a.out' $run $f 'kgbm' $t $d $g $c
 done
 
 # scratch pad
