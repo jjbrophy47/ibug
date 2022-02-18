9b9f31b86cb023d10f238832b83462258c24edc5
jbrophy <jonathanbrophy47@gmail.com>
2022-02-18 14:25:19 -0800

    updating remove script

--- scripts/postprocess/remove.sh
+++ scripts/postprocess/remove.sh
@@ -7,10 +7,13 @@ dataset_list=('ames' 'bike' 'california' 'communities' 'concrete' 'energy'
               'facebook' 'kin8nm' 'life' 'meps' 'msd' 'naval' 'news' 'obesity'
               'power' 'protein' 'star' 'superconductor' 'synthetic' 'wave'
               'wine' 'yacht')
+metric_list=('nll' 'crps')
 fold_list=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
 
 for dataset in ${dataset_list[@]}; do
-    for fold in ${fold_list[@]}; do
-        rm -rf ${in_dir}/${dataset}/fold${fold}/${method}
+    for metric in ${metric_list[@]}; do
+        for fold in ${fold_list[@]}; do
+            rm -rf ${in_dir}/${dataset}/${metric}/fold${fold}/${method}
+        done
     done
 done
