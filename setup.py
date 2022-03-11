923cf94956e8ea93536ceeaacf16002f3a064519
jbrophy <jonathanbrophy47@gmail.com>
2022-03-11 14:53:37 -0800

    bumping package version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if os.name == 'posix':
     libraries.append('m')
 
 setup(name="ibug",
-      version="0.0.1",
+      version="0.0.2",
       description="Instance-Based Uncertainty Estimation for Gradient-Boosted Regression Trees",
       author="Jonathan Brophy",
       author_email="jonathanbrophy47@gmail.com",
