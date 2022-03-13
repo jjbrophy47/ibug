d9b6c6c6ed8016c45f02174e39505b044439052f
jbrophy <jonathanbrophy47@gmail.com>
2022-03-13 12:08:44 -0700

    adding readme badges

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 IBUG: Instance-Based Uncertainty Estimation for Gradient-Boosted Regression Trees
 ---
+[![PyPi version](https://img.shields.io/pypi/v/ibug)](https://pypi.org/project/ibug/)
+[![Python version](https://img.shields.io/badge/python-3.9%20%7C%203.10-blue)](https://pypi.org/project/ibug/)
+[![Github License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://github.com/jjbrophy47/ibug/blob/master/LICENSE)
+[![Build](https://github.com/jjbrophy47/ibug/actions/workflows/wheels.yml/badge.svg?branch=v0.0.4)](https://github.com/jjbrophy47/ibug/actions/workflows/wheels.yml)
 
 **ibug** is a simple wrapper that extends *any* gradient-boosted regression trees (GBRT) model into a probabilistic estimator, and is compatible with all major GBRT frameworks including LightGBM, XGBoost, CatBoost, and SKLearn.
 
@@ -22,3 +26,14 @@ Experiment
 python3 scripts/experiments/train.py --tree_type lgb
 python3 scripts/experiments/predict.py --tree_type lgb
 ```
+
+License
+---
+[Apache License 2.0](https://github.com/jjbrophy47/ibug/blob/master/LICENSE).
+
+<!--Reference
+---
+Brophy and Lowd. [Instance-Based Uncertainty Estimation for Gradient-Boosted Regression Trees](). arXiv 2022.
+
+```
+```-->
---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+import numpy as np
+from Cython.Build import cythonize
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+ext_modules = ['ibug/parsers/_tree32.pyx',
+               'ibug/parsers/_tree64.pyx']
+
+libraries = []
+if os.name == 'posix':
+    libraries.append('m')
+
+setup(name="ibug",
+      version="0.0.4",
+      description="Instance-Based Uncertainty Estimation for Gradient-Boosted Regression Trees",
+      author="Jonathan Brophy",
+      author_email="jonathanbrophy47@gmail.com",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/jjbrophy47/ibug",
+      packages=find_packages(),
+      include_package_data=True,
+      package_dir={"ibug": "ibug"},
+      classifiers=["Programming Language :: Python :: 3.9",
+                   "Programming Language :: Python :: 3.10",
+                   "License :: OSI Approved :: Apache Software License",
+                   "Operating System :: OS Independent"],
+      python_requires='>=3.9',
+      install_requires=["numpy>=1.21"],
+      ext_modules=cythonize(ext_modules,
+                            compiler_directives={'language_level': 3},
+                            annotate=True),
+      include_dirs=np.get_include(),
+      zip_safe=False)
